4e3513a112f165b5f51a983fe59549a28e417a44
Rizky Eka Haryadi <127828164+Dyanrangga13@users.noreply.github.com>
2023-06-05 22:11:03 +0700

    first commit

---
+++ pertemuan8_rangga/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pertemuan8_rangga;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write('\n');
+        Person person1 = new Person();
+        person1.Name = "Dyan rangga saputra";
+        person1.Age = 19;
+        person1.infoPerson();
+        Person person2 = new Person("Rangga", 19);
+
+
+        Karyawan karyawan1 = new Karyawan();
+        karyawan1.karyawanID = 22114864;
+        karyawan1.subject = "Dyan Rangga";
+        karyawan1.infoKaryawan();
+
+        Karyawan karyawan2 = new Karyawan("Engineering");
+    }
+}
+
